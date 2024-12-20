f8321d2fd8c8cf4513432dbe3c245450ff0bc0bb
Priyanka0603200 <68949270+Priyanka0603200@users.noreply.github.com>
2024-12-20 14:59:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ ingredients_list=st.multiselect(
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
